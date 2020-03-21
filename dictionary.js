78025b75bc33ce78871a0eb9e57b08d5ea16dcfa
personaldata143 <62426940+personaldata143@users.noreply.github.com>
2020-03-21 15:45:59 +0300

    adding new test library

--- dictionary.js
+++ dictionary.js
@@ -8,9 +8,6 @@ git push https://github.com/personaldata143/Memento.git
 var ds = new SampleDS();
 var r = ds.search(query);
 result( r , function(id) { return ds.extra(id);});
-  if (res[0].meta.id !== undefined)
-  result['word'] = res[0].meta.id;
-    
 @apicall
 https://www.dictionaryapi.com/api/v3/references/collegiate/json/tall?key=20092269-c22f-46c2-a8c0-3bf0c4a9f097
 */
@@ -25,10 +22,8 @@ SampleDS.prototype.search = function(q) {
     
   var resultJson = http().get(baseurl + encodeURIComponent(q) + "?key=" + this.apikey );
   var res = JSON.parse(resultJson.body);
-
-  var result = {};
+  var result = {test:0};
     
-
   return result;
 
 }
---
+++ weather.js
@@ -0,0 +1,54 @@
+/*
+Library for a custom data source for Memento DataBase
+to obtaining information from the api at https://openweathermap.org.
+See https://openweathermap.org/api.
+@param {string} apiKey -- api key.
+@example
+var openweather = new Openweather("Api key" );
+var r = openweather.search(query);
+result( r , function(id) { return openweather.extra(id);});
+Follows example at
+https://github.com/mementodatabase/scripts/blob/master/data-sources/discogs.js
+*/
+var baseurl = 'http://api.openweathermap.org/data/2.5/weather?';
+function Openweather () {
+    this.apiKey = "a0848cdb8a693c3a2f84e029fe2423d9";
+}
+/*
+Issue a search query to openweather database.
+@param {string} coord: Search by coordinates
+coord = 'lat=35.772096&lon=-78.638614'
+*/
+Openweather.prototype.search = function(coord) {
+  var resultJson = http().get(baseurl + coord + "&appid=" + this.apiKey );
+  var res = JSON.parse(resultJson.body);
+  var result = {};
+  if (res.id !== undefined)
+      result['id'] = res['id'];
+  if (res.name !== undefined)
+      result['location'] = res['name'];
+  if (res.weather !== undefined) {
+      result['conditions'] = res.weather[0]['main'];
+      result['detail'] = res.weather[0]['description'];
+      result['icon'] = "http://openweathermap.org/img/w/" + res.weather[0]['icon'] + ".png";
+  };
+  if (res.main !== undefined) {
+      result['temperature'] = res.main['temp'];
+      result['humidity'] = res.main['humidity'];
+      result['pressure'] = res.main['pressure'];
+  };
+  if (res.wind !== undefined)
+      result['windspeed'] = res.wind['speed'];
+  if (res.clouds !== undefined)
+      result['clouds'] = res.clouds['all'];
+  if (res.rain !== undefined) {
+      result['rain'] = res.rain['3h'];
+} else {
+      result['rain'] = "No rain";
+}
+  if (res.sys !== undefined) {
+      result['sunrise'] = res.sys['sunrise'];
+      result['sunset'] = res.sys['sunset'];
+  };
+  return result;
+}
